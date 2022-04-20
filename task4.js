50d287f399d03718237debce2743206aa48d2f8a
faryadicka <faryadicka98@gmail.com>
2022-04-20 13:34:53 +0700

    Revisi & Tambahan code yang lebih simple

--- task4.js
+++ task4.js
@@ -4,7 +4,7 @@ const divideAndSort = (numb) => {
   let res = []
   const toArr = numb.toString().split('')
   for (let i = 0; i <= toArr.length - 1; i++) {
-    found = found.concat(toArr[i]).sort((a, b) => a - b)
+    found = found.concat(toArr[i]).sort()
     if (toArr[i] == 0) {
       res = res.concat(found)
       found = []
@@ -15,8 +15,26 @@ const divideAndSort = (numb) => {
     }
   }
   let filt = res.filter(numb => numb != 0).join('')
-  return parseInt(filt)
+  return Number(filt)
 }
 
 const hasil = divideAndSort(5956560159466056)
 console.log(hasil)
+
+/* == Revisi atau program buatan baru == */
+
+const divideAndSortRevisi = (numb) => {
+  if (typeof numb !== 'number') return `Input numb harus berupa Number!`
+  let res = []
+  const toArr = numb.toString().split('0')
+  toArr.forEach(item => {
+    const arrNumb = item.split("")
+    const sort = arrNumb.sort().join("")
+    res.push(sort)
+  })
+
+  return Number(res.join(""))
+}
+
+const hasilRevisi = divideAndSortRevisi(5956560159466056)
+console.log(hasilRevisi)
